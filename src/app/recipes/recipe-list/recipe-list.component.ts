4a3a896e111586c7dddb30818c261288a104b946
Dmytro Tarasov <dtarasov24s@gmail.com>
2022-07-26 23:39:27 +0300

    Module 6 Components & DataBinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,23 @@ import { Recipe } from '../recipe.model';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
     recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://www.expatica.com/app/uploads/sites/17/2020/10/austrian-food.jpg'),
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://www.expatica.com/app/uploads/sites/17/2020/10/austrian-food.jpg')
+        new Recipe('A Test Recipe', 
+            'Description1', 
+            'https://www.expatica.com/app/uploads/sites/17/2020/10/austrian-food.jpg'),
+        new Recipe('A Test Recipe2', 
+            'Description2', 
+            'https://www.expatica.com/app/uploads/sites/17/2020/10/austrian-food.jpg')
     ];
 
     constructor() { }
 
     ngOnInit() {
+    }
+
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
     }
 
 }
