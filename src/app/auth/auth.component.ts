c60ae00dd7462add4aff8e47c80bc045be7a29ae
Dmytro Tarasov <dtarasov24s@gmail.com>
2022-08-14 12:22:05 +0300

    Module 21 Dynamic Components

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder.directive';
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
-    styleUrls: ['./auth.component.css']
+    styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
     isLoginMode = true;
     isLoading = false;
     error: string = null;
+    // ViewChild here will find the place in the template (DOM) where the PlaceholderDirective is used for the first time
+    @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective; 
 
-    constructor(private authService: AuthService, private router: Router) { }
+    private closeSub: Subscription;
 
-    ngOnInit(): void {}
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private componentFactoryResolver: ComponentFactoryResolver
+    ) { }
+
+    ngOnInit(): void { }
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -40,7 +50,7 @@ export class AuthComponent implements OnInit {
         }
 
         // deprecated variant
-        
+
         // authObs.subscribe(responseData => {
         //     console.log(responseData);
         //     this.isLoading = false;
@@ -57,10 +67,38 @@ export class AuthComponent implements OnInit {
             },
             error: (errorMessage) => {
                 this.error = errorMessage;
+                this.showErrorAlert(errorMessage);
                 this.isLoading = false;
-            }
+            },
         });
 
         authForm.reset();
+    }
+
+    onHandleError() {
+        this.error = null;
+    }
+
+    private showErrorAlert(message: string) {
+        const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        const hostViewContainerRef = this.alertHost.viewContainerRef; // 'alertHost' is a custom directive - PlaceholderDirective which we have created manually. This directive also exposes the viewContainerRef property (publicly defined in the constructor)
+
+        hostViewContainerRef.clear(); // clear all content that was rendered in that hostViewContainerRef before
+
+        // create a new instance of an AlertComponent dynamically (programmatically)
+        const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+
+        componentRef.instance.message = message;
+        this.closeSub = componentRef.instance.close.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+        });
+    }
+
+    // here, we call unsubscribe() to avoid memory leaks if we managed somehow leave the AuthComponent when the AlertComponent was shown (but this is actually impossible here in our app) 
+    ngOnDestroy() {
+        if (this.closeSub) {
+            this.closeSub.unsubscribe();
+        }
     }
 }
